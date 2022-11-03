3789c79c34650b5a1b193b3d6f028bb4d0cfc28d
dlazorchyk <dlazorchyk@betacom.com.pl>
2022-11-03 20:47:23 +0100

    General Code Strucrure Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './shared/book/book.component';
+import { BookItemComponent } from './shared/book-item/book-item.component';
+import { SearchComponent } from './shared/search/search.component';
+import { EditBookComponent } from './books/edit-book/edit-book.component';
+import { BookDetailComponent } from './books/book-detail/book-detail.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BooksListComponent } from './books/books-list/books-list.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookItemComponent,
+    SearchComponent,
+    EditBookComponent,
+    BookDetailComponent,
+    BookListComponent,
+    BooksListComponent,
+    HeaderComponent,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
